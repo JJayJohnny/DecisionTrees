73c2a89f19a4ddb46ddacef98effb79389b84355
JJayJohnny <jasiek.barczewski@gmail.com>
2023-05-20 21:34:35 +0200

    added another version of id3 algorithm that is more similar to c4.5

---
+++ id3New.py
@@ -0,0 +1,133 @@
+import numpy as np
+import pandas as pd
+import random
+
+class id3Node:
+    def __init__(self, min_samples_split=2, max_depth=None, seed=2, verbose=False):
+        # Sub nodes -- recursive, those elements of the same type (TreeNode)
+        self.children = {}
+        self.decision = None
+        self.split_feat_name = None  # Splitting feature
+        self.threshold = None  # Where to split the feature
+
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.seed = seed  # Seed for random numbers
+        self.verbose = verbose  # True to print the splits
+
+    def recursiveGenerateTree(self, xTrain, yTrain, current_depth):
+        if len(yTrain.unique()) == 1:
+            self.decision = yTrain.unique()[0]
+        elif len(yTrain) < self.min_samples_split:
+            self.decision = self.getMajClass(yTrain)
+        elif current_depth == self.max_depth:
+            self.decision = self.getMajClass(yTrain)
+        else:
+            best_attribute, best_threshold, splitter = self.splitAttribute(xTrain, yTrain)
+            self.children = {}
+            self.split_feat_name = best_attribute
+            self.threshold = best_threshold
+            current_depth += 1
+
+            for v in splitter.unique():
+                # v is 'greater' or 'lesser'
+                index = splitter == v
+                if len(xTrain[index]) > 0:
+                    self.children[v] = id3Node(min_samples_split=self.min_samples_split, max_depth=self.max_depth, seed=self.seed, verbose=self.verbose)
+                    self.children[v].recursiveGenerateTree(xTrain[index], yTrain[index], current_depth)
+                else:
+                    #
+                    self.children[v] = id3Node(min_samples_split=self.min_samples_split, max_depth=1, seed=self.seed, verbose=self.verbose)
+                    self.children[v].recursiveGenerateTree(xTrain, yTrain, current_depth=1)
+
+    def splitAttribute(self, xTrain, yTrain):
+        info_gain_max = -1 * float("inf")  # Info gain set to a minimun
+
+        splitter = pd.Series(dtype='str')
+        best_attribute = None
+        best_threshold = None
+
+        for attribute in xTrain.keys():
+            # only for continuous attributes
+            sorted_index = xTrain[attribute].sort_values(ascending=True).index
+            sorted_sample_data = xTrain[attribute][sorted_index]
+            sorted_sample_target = yTrain[sorted_index]
+
+            aig = self.compute_info_gain(xTrain[attribute], yTrain)
+
+            if aig > info_gain_max:
+                splitter = xTrain[attribute]
+                info_gain_max = aig
+                best_attribute = attribute
+                best_threshold = None
+        return best_attribute, best_threshold, splitter
+
+    def compute_entropy(self, sampleSplit):
+        #If there is only only one class, the entropy is 0
+        if len(sampleSplit) < 2:
+            return 0
+        else:
+            freq = np.array(sampleSplit.value_counts(normalize=True))
+            return -(freq * np.log2(freq + 1e-6)).sum()
+
+    def compute_info_gain(self, sampleAttribute, sample_target):
+
+        values = sampleAttribute.value_counts(normalize=True)
+        split_ent = 0
+
+        # Iterate for each class of the sample attribute
+        for v, fr in values.items():
+
+            index = sampleAttribute == v
+            sub_ent = self.compute_entropy(sample_target[index])
+
+            # Weighted sum of the entropies
+            split_ent += fr * sub_ent
+
+        # Compute the entropy without any split
+        ent = self.compute_entropy(sample_target)
+        return ent - split_ent
+
+    def getMajClass(self, yTrain):
+
+        freq = yTrain.value_counts().sort_values(ascending=False)
+
+        # Select the name of the class (classes) that has the max number of records
+        MajClass = freq.keys()[freq == freq.max()]
+        # If there are two classes with equal number of records, select one randomly
+        if len(MajClass) > 1:
+            decision = MajClass[random.Random(self.seed).randint(0, len(MajClass) - 1)]
+        # If there is only onle select that
+        else:
+            decision = MajClass[0]
+        return decision
+
+    def predict(self, sample):
+
+        # If there is a decision in the node, return it
+        if self.decision is not None:
+            return self.decision
+
+        # If not, it means that it is an internal node
+        else:
+            attr_val = sample[self.split_feat_name]
+
+            # only for discrete values
+            if self.children.__contains__(attr_val):
+                child = self.children[attr_val]
+            else:
+                return None
+
+            return child.predict(sample)
+
+    def evaluate(self, xTest, yTest):
+        correct_preditct = 0
+        wrong_preditct = 0
+        for index in range(xTest.shape[0]):
+            result = self.predict(xTest.iloc[index])
+            if result == yTest.iloc[index]:
+                correct_preditct += 1
+            else:
+                wrong_preditct += 1
+        accuracy = correct_preditct / (correct_preditct + wrong_preditct)
+        return accuracy
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import id3
 import c45
+import id3New
 from sklearn.model_selection import train_test_split
 import time
 
@@ -15,20 +16,32 @@ if __name__ == '__main__':
     data = GetData(dataLoc)
     data = data.head(1000)
     xTrain, xTest, yTrain, yTest = train_test_split(data.drop('is_safe', axis=1), data['is_safe'], test_size=0.2)
-    startID3 = time.time()
+    start = time.time()
     tree = id3.id3(xTrain, yTrain)
-    endID3 = time.time()
+    stop = time.time()
     print(tree)
     print("ID3:")
     print("Accuracy on train data: "+str(id3.evaluate(tree, xTrain, yTrain)))
     print("Accuracy on test data: "+str(id3.evaluate(tree, xTest, yTest)))
-    print("Training time: "+str(endID3-startID3))
+    print("Training time: "+str(stop-start))
 
-    # print("\nC4.5:")
-    # c45 = c45.c45Node(max_depth=3)
-    # c45.recursiveGenerateTree(xTrain, yTrain, 0)
-    # print("Accuracy on train data: " + str(c45.evaluate(xTrain, yTrain)))
-    # print("Accuracy on test data: " + str(c45.evaluate(xTest, yTest)))
+    print("\nC4.5:")
+    c45 = c45.c45Node(max_depth=3)
+    start = time.time()
+    c45.recursiveGenerateTree(xTrain, yTrain, 0)
+    stop = time.time()
+    print("Accuracy on train data: " + str(c45.evaluate(xTrain, yTrain)))
+    print("Accuracy on test data: " + str(c45.evaluate(xTest, yTest)))
+    print("Training time: " + str(stop - start))
+
+    id3 = id3New.id3Node()
+    start = time.time()
+    id3.recursiveGenerateTree(xTrain, yTrain, 0)
+    stop = time.time()
+    print("ID3:")
+    print("Accuracy on train data: " + str(id3.evaluate(xTrain, yTrain)))
+    print("Accuracy on test data: " + str(id3.evaluate(xTest, yTest)))
+    print("Training time: " + str(stop - start))
 
 
 
